653ed5aec271061d0ebaf3c8535aeab5ca79fb95
iris <109429124+archiristo@users.noreply.github.com>
2024-11-14 15:48:37 +0300

    Add files via upload

---
+++ HelloCpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	cout<<"Hello World"<<endl;
+	char name='i';
+	cout<<name<<endl;
+	int num1=27;
+	cout<<num1<<endl;
+	double num2=1.905;
+	cout<<num2<<endl;
+	string admin="arch";
+	cout<<admin<<endl;
+	string nick1="arch",nick2="iristo";
+	cout<<"admin: "<<nick1<<nick2<<endl;
+	
+}
