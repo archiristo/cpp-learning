ab2577470af75910be949002247f24c47608ef52
iris <109429124+archiristo@users.noreply.github.com>
2024-11-14 15:55:01 +0300

    Add files via upload

---
+++ firstInput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+	string nickname;
+	cout<<"Enter your nickname: "<<endl;
+	cin>>nickname;
+	cout<<"Welcome "<<nickname<<endl;
+}
